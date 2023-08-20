d3493aa2bd2b1dbeb39af8c3c64c526c8e8fbbea
rajnikam846 <rajnikam846@gmail.com>
2023-08-20 16:10:21 +0530

    Added the code

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
-/*terraform {
+terraform {
   backend "s3" {
     bucket         = "sampesamsungjhooq-terraform-s3-buket1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
 }
-*/
