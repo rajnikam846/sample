5ddacb0349e0c33266cecffe51c83b77d822dba9
rajnikam846 <rajnikam846@gmail.com>
2023-08-20 17:55:02 +0530

    Added the file path to terraform provider

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@
 
 provider "aws"{
     region = "us-east-1"
-    shared_credentials_files = ["/root/.aws/credentials"]
+    shared_credentials_files = "/root/.aws/credentials"
     /* access_key = "AKIAYGGTO5KKV6ZHX756"
     secret_key = "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" */
 }
