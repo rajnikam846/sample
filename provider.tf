6c806c5604ba2ce7120530410871e75f6309a898
rajnikam846 <rajnikam846@gmail.com>
2023-08-20 17:09:32 +0530

    Added the code

--- provider.tf
+++ provider.tf
@@ -1,14 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket         = "sampesamsungjhooq-terraform-s3-buket1"
-    key            = "terraform"
-    region         = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "sampesamsungjhooq-terraform-s3-buket1"
+#     key            = "terraform"
+#     region         = "us-east-1"
+#   }
+# }
 
 
 provider "aws"{
     region = "us-east-1"
-    access_key = "AKIAYGGTO5KKV6ZHX756"
-    secret_key = "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"
 }
