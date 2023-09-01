9988f487f5fc50e299d0e5137d21758ca3c687e8
rajnikam846 <142670499+rajnikam846@users.noreply.github.com>
2023-09-01 12:20:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,5 @@ provider "aws"{
     region = "us-east-1"
     profile = "default"
     #shared_credentials_files = "/root/.aws/credentials"
-    #access_key = "AKIAYGGTO5KKV6ZHX756"
-    #secret_key = "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"
+   
 }
